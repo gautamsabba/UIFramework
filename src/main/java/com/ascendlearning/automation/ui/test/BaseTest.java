f631f5dac497cc26e5e3893dba9bec3c933e3af1
gautamsabba <gautam.sabba@gmail.com>
2015-06-07 19:22:09 -0500

    no message

--- src/main/java/com/ascendlearning/automation/ui/test/BaseTest.java
+++ src/main/java/com/ascendlearning/automation/ui/test/BaseTest.java
@@ -1,5 +1,7 @@
 package com.ascendlearning.automation.ui.test;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -10,11 +12,15 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 
 import com.ascendlearning.automation.ui.config.GlobalProperties;
+import com.ascendlearning.automation.ui.config.PropertiesRepository;
 import com.ascendlearning.automation.ui.driver.DriverFactory;
+import com.ascendlearning.automation.ui.exceptions.DriverException;
 
 public class BaseTest {
 	
 	protected WebDriver driver = null;
+	
+	private Logger logger = LogManager.getLogger(this.getClass());
 	
 	@BeforeMethod
 	public void setup() {
@@ -24,6 +30,21 @@ public class BaseTest {
 	@AfterMethod
 	public void tearDown() {
 		DriverFactory.getInstance().removeDriver();
+	}
+	
+	/**
+	 * Load the properties file from the classpath
+	 * 
+	 * e.g. loadProperties("jblearning.properties");
+	 * @param propFile
+	 * @throws DriverException 
+	 */
+	protected void loadProperties(String propFile) {
+		try {
+			PropertiesRepository.appendProperties(propFile);
+		} catch (DriverException e) {
+			logger.error("Unable to load properties file : " + propFile, e);
+		}
 	}
 
 }
