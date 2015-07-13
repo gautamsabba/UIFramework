860f47c0d7117e2059669024b7574d513cc9fa52
gautamsabba <gautam.sabba@gmail.com>
2015-07-13 18:16:46 +0530

    Added ability to handle frames in WindowHandler

--- src/main/java/com/ascendlearning/automation/ui/handlers/WindowHandler.java
+++ src/main/java/com/ascendlearning/automation/ui/handlers/WindowHandler.java
@@ -1,8 +1,10 @@
 package com.ascendlearning.automation.ui.handlers;
 
 import java.util.Iterator;
+import java.util.List;
 
 import org.apache.log4j.Logger;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
@@ -47,13 +49,45 @@ public class WindowHandler extends BaseHandler {
     	return driver;
     }
     
-	public WebElement switchToModalDialog(String... waitFor) {
+	public WebElement switchToModalDialog(String...waitFor) {
 		WebElement activeElement = driver.switchTo().activeElement();
 		logger.info("Switching to active element : " + activeElement.getText());
 		if (waitFor != null && waitFor.length > 0) {
 			setDriverWait(driver, waitFor[0]);
 		}
 		return activeElement;
+	}
+	
+	public WebDriver switchToFrame(int index, String...waitFor) {
+		currentDriver = driver.switchTo().frame(index);
+		logger.info("Switching to frame : " + driver.getTitle());
+		if (waitFor != null && waitFor.length > 0) {
+			setDriverWait(driver, waitFor[0]);
+		}
+    	return driver;
+	}
+	
+	public WebDriver switchToFrame(String id, String...waitFor) {
+		currentDriver = driver.switchTo().frame(id);
+		logger.info("Switching to frame : " + driver.getTitle());
+		if (waitFor != null && waitFor.length > 0) {
+			setDriverWait(driver, waitFor[0]);
+		}
+    	return driver;
+	}
+	
+	public String[] listFrames(String...waitFor) {
+		final List <WebElement> iframes = driver.findElements(By.tagName("iframe"));
+		int size = iframes.size();
+		String[] frames = null;;
+	    for (int i=0 ; i < size; i++) {
+	    	frames = new String[size];
+	    	String id = iframes.get(i).getAttribute("id");
+	        if (!id.equals(null) && id.length() > 0) {
+	        	frames[i] = iframes.get(i).getAttribute("id");
+	        }
+	    }
+	    return frames;
 	}
 
 	public void cleanUp(WebDriver webDriver) {
