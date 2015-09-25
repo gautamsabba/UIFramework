7f72ba9a3af4383c966fe5890f6baaa327e3bead
Gautam Sabba <gautam.sabba@gmail.com>
2015-09-25 21:29:15 +0530

    Added license and copyright information

--- src/main/java/com/codescaping/automation/ui/utils/ExcelHandler.java
+++ src/main/java/com/codescaping/automation/ui/utils/ExcelHandler.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010-2011 Ardesco Solutions - http://www.ardescosolutions.com
+ * Copyright (c) 2010-2015 Ardesco Solutions - http://www.ardescosolutions.com
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
--- src/main/java/com/codescaping/automation/ui/utils/filedownloader/FileDownloader.java
+++ src/main/java/com/codescaping/automation/ui/utils/filedownloader/FileDownloader.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010-2012 Lazery Attack - http://www.lazeryattack.com
+ * Copyright (c) 2010-2015 Lazery Attack - http://www.lazeryattack.com
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
